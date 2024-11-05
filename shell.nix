45b3ea63b8f8de5367e06f984e53a23e25da0898
Kamen Mladenov <kamen@syndamia.com>
2024-11-05 13:49:53 +0200

    feat(shell.nix): Improve pathing

--- shell.nix
+++ shell.nix
@@ -8,7 +8,6 @@ pkgs.mkShell {
     pkgs.z3_4_12
   ];
   shellHook = ''
-    z3_path=$(which z3)
-    export VERUS_Z3_PATH="$z3_path"
+    export PATH="$PATH:${builtins.toString ./.}/source/target-verus/debug:${builtins.toString ./.}/source/target-verus/release:${pkgs.z3_4_12}/bin"
   '';
 }
