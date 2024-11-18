9d473ef16fdfa4c971e7eb44a6bb91d129502101
Aristotelis Papanis <aristotelis.papanis@blocksense.network>
2024-11-18 15:04:09 +0200

    Update nix shell to allow better nix packaging

--- shell.nix
+++ shell.nix
@@ -8,6 +8,11 @@ pkgs.mkShell {
     pkgs.z3_4_12
   ];
   shellHook = ''
+    z3_path=$(which z3)
+    export VERUS_Z3_PATH="$z3_path"
+    export RUSTC_BOOTSTRAP=1
+    export VERUS_IN_VARGO=1
+    export RUSTFLAGS="--cfg proc_macro_span --cfg verus_keep_ghost --cfg span_locations"
     export PATH="$PATH:${builtins.toString ./.}/source/target-verus/debug:${builtins.toString ./.}/source/target-verus/release:${pkgs.z3_4_12}/bin"
   '';
 }
